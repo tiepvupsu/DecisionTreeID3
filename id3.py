eb4ed5a7e433ca7478857dabe6798837c2ce9c5b
Tiep Vu <vuhuutiep@gmail.com>
2018-01-13 16:01:06 -0500

    minor clean up

--- id3.py
+++ id3.py
@@ -6,13 +6,12 @@ import pandas as pd
 class TreeNode(object):
     def __init__(self, ids = None, children = [], entropy = 0, depth = 0):
         self.ids = ids           # index of data in this node
-        self.children = children # list of its child nodes
-
         self.entropy = entropy   # entropy, will fill later
         self.depth = depth       # distance to root node
-        self.split_attribute = None
-        self.order = None  # each node corresponding to what value of attribute
-        self.label = None
+        self.split_attribute = None # which attribute is chosen, it non-leaf
+        self.children = children # list of its child nodes
+        self.order = None       # order of values of split_attribute in children
+        self.label = None       # label of node if it is a leaf
 
     def set_properties(self, split_attribute, order):
         self.split_attribute = split_attribute
@@ -56,21 +55,19 @@ class DecisionTreeID3(object):
             else:
                 self._set_label(node)
                 
-        
-                
     def _entropy(self, ids):
         # calculate entropy of a node with index ids
         if len(ids) == 0: return 0
         ids = [i+1 for i in ids]
-        prob = np.array(self.target[ids].value_counts())
-        # print(prob, entropy(prob))
-        return entropy(prob)
+        # remove 0 freq since log is not defined at 0 
+        freq = np.array(self.target[ids].value_counts())
+        return entropy(freq)
 
     def _set_label(self, node):
+        # find label for a node if it is a leaf
+        # simply chose by major voting 
         target_ids = [i + 1 for i in node.ids]  # target is a series variable
-        node.set_label(self.target[target_ids].mode()[0])  # most frequent label
-
-
+        node.set_label(self.target[target_ids].mode()[0]) # most frequent label
     
     def _split(self, node):
         ids = node.ids 
@@ -86,13 +83,14 @@ class DecisionTreeID3(object):
             for val in values: 
                 sub_ids = sub_data.index[sub_data[att] == val].tolist()
                 splits.append([sub_id-1 for sub_id in sub_ids])
+            # don't split if a node has too small points
             if min(map(len, splits)) < self.min_samples_split: continue
             # information gain
             HxS= 0
             for split in splits:
                 HxS += len(split)*self._entropy(split)/len(ids)
             gain = node.entropy - HxS 
-            if gain < self.min_gain: continue 
+            if gain < self.min_gain: continue # stop if small gain 
             if gain > best_gain:
                 best_gain = gain 
                 best_splits = splits
@@ -111,12 +109,13 @@ class DecisionTreeID3(object):
         npoints = new_data.count()[0]
         labels = [None]*npoints
         for n in range(npoints):
-            data1 = new_data.iloc[n, :]
+            x = new_data.iloc[n, :] # one point 
+            # start from root and recursively travel if not meet a leaf 
             node = self.root
-            while node.children:
-                node = node.children[node.order.index(data1[node.split_attribute])]
-
+            while node.children: 
+                node = node.children[node.order.index(x[node.split_attribute])]
             labels[n] = node.label
+            
         return labels
 
 if __name__ == "__main__":
